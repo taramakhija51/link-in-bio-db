0a14b5d2f4aa503106b60e3d3e8129b79d4fe427
taramakhija51 <114837329+taramakhija51@users.noreply.github.com>
2024-11-03 00:41:33 +0000

    fixed naming

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,4 @@
-class ItemController < ApplicationController
+class ItemsController < ApplicationController
   def index
     @list_of_items = Item.all
     render({ :template => "item_templates/list" })
